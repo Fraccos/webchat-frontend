087a115166646c1c1bd78aaf5f06f625d7804480
Fraccos <135456522+Fraccos@users.noreply.github.com>
2023-06-06 11:17:05 +0200

    Develop Routes (#5)
    * Initial commit. Environment setup
    
    * Restyled environment.
    
    * Updated environment
    
    * clean up default css
    
    * first commit #1
    
    * close issue #1
    
    ---------
    
    Co-authored-by: Qwertyadmin <Qwertyadmin@users.noreply.github.com>
    Co-authored-by: verazizzo <verazizzo@users.noreply.github.com>
    Co-authored-by: Fraccos <Fraccos@users.noreply.github.com>

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Routes from './Routes';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes></Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,16 @@
+
+
+interface LoginProps {
+    
+}
+
+const LoginPage: React.FC<LoginProps> = (props) => {
+  return (
+    <>
+        <h1>Hello World</h1>
+    </>
+  )
+    
+}
+
+export default LoginPage;
