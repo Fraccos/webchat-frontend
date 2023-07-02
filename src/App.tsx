892f84be242eeb0415f967c1dd8adee5ae17ff5f
Fraccos <Fraccos@@users.noreply.github.com>
2023-07-02 11:30:55 +0200

    handle group, chat , avatar and fixed some socket event on chat

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,10 @@ import { useNavigate } from 'react-router-dom';
 import { socket } from './services/socket';
 import { Socket, io } from 'socket.io-client';
 import { Chatroom, Message } from './types/Chatroom';
-import { Box, CircularProgress } from '@mui/material';
+import { Alert, AlertColor, Box, CircularProgress, Snackbar } from '@mui/material';
+import NotificationService, { NotifyMessage, notifyCallback } from './services/NotificationService';
+import NewChatroom from './components/modals/NewChatroomModal';
+import { FriendshipRequest } from './types/FriendshipRequest';
 
 
 function App() {
@@ -26,6 +29,50 @@ function App() {
   const [isSocketConnected, setSocketConnected] = useState(false);
 
   const [chatrooms, setChatrooms] = useState<Chatroom[]>([])
+  const [usernamesMap, setUsernamesMap] = useState<any>({})
+
+  const [notification, setNotification] = useState<NotifyMessage>();
+  const [showNotication, setShowNotification] = useState(false); 
+  const [pendingReq, setPendingReq] = useState<FriendshipRequest[]>([]);
+
+  const addNotification:notifyCallback = (n: NotifyMessage) => {
+    setNotification(n);
+    setShowNotification(true);
+  }
+
+  useEffect(()=>{
+    NotificationService.serveAsDispachter(addNotification);
+  },[])
+  
+  useEffect(()=>{
+    //Verifica se dispone delle info (gli username)
+    const missingUserSet = new Set<string>();
+    chatrooms.forEach( (chat)=>{
+      if (chat.members !== undefined) {
+        chat.members.forEach( (uId) => {
+          if (uId !== undefined) {
+            if (usernamesMap[uId] === undefined) {
+              missingUserSet.add(uId);
+            }
+          }
+        })
+      }
+    })
+    /* Sono consapevole di usare in modo non strettamente proprio la post perchè la mia azione è idempotente
+    perchè si tratta di un retrive, ma d'altro canto dato che devo passare un array dinamico dimensione ignota a priori
+    perchè sia meglio passarlo come body e non come URLparams quindi opto per una posto */
+    if (missingUserSet.size > 0) {
+      cAPIWrapper.post("/users/usernamesMap/retrive", {
+        data: {
+          idArray: Array.from(missingUserSet)
+        }
+      }).then( (obj) => {
+        setUsernamesMap( (prev:any) => {
+          return {...prev, ...obj.data}
+        })
+      })
+    }
+  }, [chatrooms])
 
   const handleUserUpdate = (newUser:User, jwt: string) => {
     setCurrentUser(newUser);
@@ -48,8 +95,29 @@ function App() {
 
   const onChatroomCreated = (data: any) => {
     const newChatroom = data as Chatroom;
-    console.log(data);
-    setChatrooms([...chatrooms, newChatroom]);
+    let content = newChatroom.type === "group" ?
+       <>Sei stato aggiunto al gruppo <strong>{newChatroom.name}</strong></>
+        :
+      <><strong>{newChatroom.name}</strong> ha iniziato una conversazione con te</>;
+    NotificationService.push({
+      type: "success",
+      content: content,
+      insertionDate: new Date()
+    })
+    setChatrooms( (prev) => [...prev, newChatroom]);
+  }
+
+  const onChatroomDeleted = (data: any) => {
+    const delChatroom = data;
+    setChatrooms(  (prev) => {
+      const _del = prev.find(c => c._id.toString() === delChatroom.id.toString());
+      NotificationService.push({
+        type: "warning",
+        content: <>La chatroom <strong>{_del?.name}</strong> è stata eliminata</>,
+        insertionDate: new Date()
+      })
+      return prev.filter(c => c._id.toString() !== delChatroom.id.toString());
+    }) 
   }
 
   const initSocket = () => {
@@ -71,17 +139,19 @@ function App() {
     socket.on('connect', onConnect);
     socket.on('disconnect', onDisconnect);
     socket.on('pushedMessage', onPushedMessage);
-    socket.on('chatroomCreated', onChatroomCreated)
+    socket.on('chatroomDeleted', onChatroomDeleted);
+    socket.on('chatroomCreated', onChatroomCreated);
+    // cAPIWrapper.on('error', )
     return () => {
       socket.off('connect', onConnect);
       socket.off('disconnect', onDisconnect);
       socket.off('pushedMessage', onPushedMessage);
       socket.off('chatroomCreated', onChatroomCreated)
+      socket.off('chatroomDeleted', onChatroomDeleted);
     };
   }
 
   const initUser = () => {
-    setLoading(true);
     cAPIWrapper.post("/users/jwt/create").then(
       (res) => {
         handleUserUpdate(res.data.user, res.data.token);
@@ -108,8 +178,9 @@ function App() {
     if (JWT.length > 0) {
       cAPIWrapper.get("/chats/latestedited/0").then((res)=> {
         try {
-          const chats = res.data as Chatroom[]
+          const chats = res.data.chatrooms as Chatroom[]
           setChatrooms( chats );
+          setUsernamesMap(res.data.usernames);
           initSocket();
         } catch (e) {
           console.error(e);
@@ -122,12 +193,19 @@ function App() {
 
   return (
     <div className="App">
+        <Snackbar open={showNotication} onClose={()=>setShowNotification( prev => !prev)} anchorOrigin={{vertical:"bottom", horizontal: "right" }} autoHideDuration={10000}>
+          {notification && <Alert severity={notification.type as AlertColor} sx={{ width: '100%' }}>
+            {notification.content}
+          </Alert>}
+        </Snackbar>
+      
       {!isLoading ? <Routes> 
         <Route path="/:chatid?" element={
           <HomePage
             chatrooms={chatrooms}
             requireAuth={requireAuth}
             user={currentUser}
+            usernamesMap={usernamesMap}
           />} />
         <Route path="/login" element={<LoginPage handleUserUpdate={handleUserUpdate}/>} />
         <Route path="/register" element={<RegisterPage />}/>
